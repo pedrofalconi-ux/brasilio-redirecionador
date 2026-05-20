6764e06597079116b6f29432fee5154d76966f59
pedrofalconi-ux <pedrofalconi11@gmail.com>
2026-05-20 12:19:29 -0300

    chore: remove redundant header bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import { Header }         from '@/app/components/Header'
 import { ProfileSection } from '@/app/components/ProfileSection'
 import { LinksNav }        from '@/app/components/LinksNav'
 
 export default function Home() {
   return (
     <div className="brand-bg min-h-dvh flex flex-col items-center">
-      <Header />
-
       <main className="w-full max-w-[480px] flex flex-col items-stretch px-5 pt-8 pb-10">
         <ProfileSection />
         <LinksNav />
